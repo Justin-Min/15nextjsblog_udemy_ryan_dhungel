cdf5c9b0e96d350738451a23f40f514b327db31e
Justin <sa3333@naver.com>
2021-01-06 22:45:50 +0900

    change

--- backend/controllers/blog.js
+++ backend/controllers/blog.js
@@ -7,6 +7,7 @@ const stripHtml = require('string-strip-html')
 const _ = require('lodash')
 const { errorHandler } = require('../helpers/dbErrorHandler')
 const fs = require('fs')
+const { smartTrim } = require('../helpers/blog')
 
 exports.create = (req, res) => {
   let form = new formidable.IncomingForm()
@@ -33,10 +34,15 @@ exports.create = (req, res) => {
     let blog = new Blog()
     blog.title = title
     blog.body = body
+    blog.excerpt = smartTrim(body, 320, ' ', ' ...')
     blog.slug = slugify(title).toLowerCase()
     blog.mtitle = `${title} | ${process.env.APP_NAME}`
     blog.mdesc = stripHtml(body.substring(0, 160))
     blog.postedBy = req.user._id
+
+    // Categories and tags
+    let arrayOfCategories = categories && categories.split(',')
+    let arrayOfTags = tags && tags.split(',')
 
     if (files.photo) {
       if (files.photo.size > 10000000)
@@ -51,7 +57,23 @@ exports.create = (req, res) => {
     blog.save((err, result) => {
       if (err) return res.status(400).json({ error: errorHandler(err) })
 
-      res.json(result)
+      // res.json(result)
+      Blog.findByIdAndUpdate(
+        result._id,
+        { $push: { categories: arrayOfCategories } },
+        { new: true }
+      ).exec((err, result) => {
+        if (err) return res.status(400).json({ error: errorHandler(err) })
+        else
+          Blog.findByIdAndUpdate(
+            result._id,
+            { $push: { tags: arrayOfTags } },
+            { new: true }
+          ).exec((err, result) => {
+            if (err) return res.status(400).json({ error: errorHandler(err) })
+            else res.json(result)
+          })
+      })
     })
   })
 }
---
+++ frontend/components/crud/BlogCreate.js
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+import { useState, useEffect } from 'react'
+import Router from 'next/router'
+import dynamic from 'next/dynamic'
+
+const CreateBlog = () => {
+  return (
+    <div>
+      <h2>Create Blog Form</h2>
+    </div>
+  )
+}
+
+export default CreateBlog
