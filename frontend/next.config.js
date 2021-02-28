4be3ce87657584b776e643439302aa7ddc84e553
Justin <sa3333@naver.com>
2021-02-28 17:49:00 +0900

    change

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -5,5 +5,8 @@ module.exports = withCSS({
     APP_NAME: 'SEOBLOG',
     API_DEVELOPMENT: 'http://localhost:8000/api',
     PRODUCTION: false,
+    DOMAIN_DEVELOPMENT: 'http://localhost:3000',
+    DOMAIN_PRODUCTION: 'https://seoblog.com',
+    FB_APP_ID: '123456',
   },
 })
